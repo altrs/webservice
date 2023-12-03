a392a31ea5a398c8f8c1cba019b5db9eaaa40af0
Robin Altman <robinhaltman@gmail.com>
2023-12-02 23:21:32 -0500

    added the rest of categories

--- index.js
+++ index.js
@@ -12,20 +12,48 @@ app.use(express.json());
 
 app.post('/submit', (req, res) => {
   console.log('POST request received at /submit');
-  const { firstNameChecked, lastNameChecked, namePrefixChecked } = req.body;
+  const { firstNameChecked, lastNameChecked, namePrefixChecked, addressChecked, cityChecked, titleChecked, emailChecked, userChecked, phoneChecked, birthdayChecked, colorChecked, agentChecked } = req.body;
   console.log('Request Body:', req.body);
 
   info = [];
 
   if(firstNameChecked){
-    info[0] = "First name: " + casual.first_name;
+    info.push("First name: " + casual.first_name);
   }
   if(lastNameChecked){
-    info[1] = "Last name: " + casual.last_name;
+    info.push("Last name: " + casual.last_name);
   }
   if(namePrefixChecked){
-    info[2] = "Name prefix: " + casual.name_prefix;
+    info.push("Name prefix: " + casual.name_prefix);
   }
+
+  if(addressChecked){
+    info.push("Address: " + casual.address);
+  }
+  if(cityChecked){
+    info.push("City: " + casual.city);
+  }
+
+  if(emailChecked){
+    info.push("Email: " + casual.email);
+  }
+  if(userChecked){
+    info.push("User: " + casual.username);
+  }
+  if(phoneChecked){
+    info.push("Phone: " + casual.phone);
+  }
+
+  if(birthdayChecked){
+    info.push("Birth year: " + casual.year);
+  }
+  if(colorChecked){
+    info.push("Favorite Color: " + casual.color_name);
+  }
+  if(agentChecked){
+    info.push("Agent: " + casual.user_agent);
+  }
+
 
   const generatedSentence = info.join('<br>');
   console.log('Generated Sentence:', generatedSentence);
@@ -33,9 +61,9 @@ app.post('/submit', (req, res) => {
   res.json({ result: generatedSentence });
 });
 
-function generateSentence(firstNameChecked, lastNameChecked, namePrefixChecked) {
-  return casual.sentence;
-}
+// function generateSentence(firstNameChecked, lastNameChecked, namePrefixChecked) {
+//   return casual.sentence;
+// }
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
